899eea3a568b70553fb7cd60fb39eccf33f4a7ad
bc96z <131604703+bc96z@users.noreply.github.com>
2025-02-04 14:53:43 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -45,6 +45,7 @@ def process_folder(folder):
             file_path = os.path.join(root, file)
             convert_audio_to_mp3(file_path)
 x=123
+y = 999
 # 執行轉換
 process_folder(root_folder)
 print("批量轉換完成！")
